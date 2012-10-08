0166115834d5c9677ba12e77a4776e0c4e7e7522
Daniel Chia <danchia@coursera.org>
2012-10-08 13:33:42 -0700

    cleanup, make IntegerTriple non-mutable

--- pa1/java/src/cs224n/wordaligner/IBMModel2.java
+++ pa1/java/src/cs224n/wordaligner/IBMModel2.java
@@ -80,21 +80,6 @@ public class IBMModel2 implements WordAligner {
     // init q(i|j,l,m) to be random
     Random random = new Random();
     probAlignment = new CounterMap<IntegerTriple, Integer>();
-    //for (SentencePair sentencePair: trainingData) {
-    //  List<String> sourceWords = sentencePair.getSourceWords();
-    //  List<String> targetWords = sentencePair.getTargetWords();
-    //  
-    //  int sourceSentenceLength = sourceWords.size() - 1;
-    //  IntegerTriple triple = new IntegerTriple(0, sourceSentenceLength, targetWords.size());
-    //  for (int j = 0; j < targetWords.size(); j++) {
-    //    triple.x = j;
-    //    
-    //    for (int i = 0; i <= sourceSentenceLength; i++)
-    //      probAlignment.setCount(triple, i, random.nextDouble() + 0.1);
-    //  }
-    //}
-    //probAlignment = Counters.conditionalNormalize(probAlignment);   // normalize
-    
     
     for (SentencePair sentencePair: trainingData) {
       List<String> targetWords = sentencePair.getTargetWords();
@@ -214,7 +199,7 @@ public class IBMModel2 implements WordAligner {
   
   // simple container class to hold a (int, int, int) set
   class IntegerTriple {
-    public int x, y, z;
+    private int x, y, z;
     
     public IntegerTriple(int x, int y, int z) {
       this.x = x;
